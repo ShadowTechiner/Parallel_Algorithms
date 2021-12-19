f0f177d43f88b0b2f4ea60a885f2d7ca4be2447e
ShadowTechiner <81445200+ShadowTechiner@users.noreply.github.com>
2021-12-19 20:16:07 +0300

    Add files via upload

---
+++ Assignment4.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include "mpi.h"
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[]) {
+	MPI_Init(&argc, &argv);
+	int rank, n, i;
+	double message;
+	double rang;
+	MPI_Status status;
+	MPI_Comm_size(MPI_COMM_WORLD, &n);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if (rank == 4)
+	{
+		cout << "Hello from process " << rank << "\n";
+		for (i = 1; i < n; i++) {
+			MPI_Recv(&message, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			cout << "Hello from process " << message << endl;
+		}
+	}
+	else 
+	{
+		rang = (double)rank;
+		MPI_Send(&rang, 1, MPI_DOUBLE, 4, 4, MPI_COMM_WORLD);
+	}
+	MPI_Finalize();
+	return 0;
+}
---
+++ Assignment5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "mpi.h"
+#define NTIMES 100
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int rank, n, i;
+	double message, rang, time_start, time_finish;
+	MPI_Init(&argc, &argv);
+	MPI_Status status;
+	MPI_Comm_size(MPI_COMM_WORLD, &n);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	time_start = MPI_Wtime();
+	for (int j = 0; j < NTIMES; j++)
+	{
+		if (rank == 4)
+		{
+			//cout << "Hello from process " << rank << "\n";
+			for (i = 1; i < n; i++) {
+				MPI_Recv(&message, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+				//cout << "Hello from process " << message << endl;
+			}
+		}
+		else
+		{
+			rang = (double)rank;
+			MPI_Send(&rang, 1, MPI_DOUBLE, 4, 4, MPI_COMM_WORLD);
+		}
+		time_finish = MPI_Wtime();
+	}
+	cout <<"rank "<<rank<<" " << "time=" << (time_finish - time_start) / NTIMES << endl;
+	MPI_Finalize();
+}
