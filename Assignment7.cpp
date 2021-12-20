d2039e336d43a3ef1bdcae8914822ac0b8144810
ShadowTechiner <81445200+ShadowTechiner@users.noreply.github.com>
2021-12-20 16:56:01 +0300

    Add files via upload

---
+++ Assignment7.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <mpi.h>
+#include <vector>
+using namespace std;
+int main(int argc, char** argv)
+{
+	int rank, size;
+	int sum = 0;
+	int sum_proc, sum_extra;
+	int residual;
+	int x, y;
+	int n = 1000000;
+	vector<int> a;
+	vector<int> b;
+	MPI_Status status;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if (rank == 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			a.push_back(1);
+		}
+		for (int i = 0; i < n; i++)
+		{
+			b.push_back(1);
+		}
+		for (int i = 1; i < size; i++)
+		{
+			MPI_Send(&a[(i-1)*n/(size-1)], n/(size-1), MPI_INT, i , 0, MPI_COMM_WORLD);
+			MPI_Send(&b[(i - 1) * n / (size - 1)], n / (size - 1), MPI_INT, i, 0, MPI_COMM_WORLD);
+			MPI_Recv(&sum_proc, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
+			sum += sum_proc;
+		}
+		residual = n % (size - 1);
+		for (int i = residual+1; i < size; i++)
+		{
+			x = 0;
+			y = 0;
+			MPI_Send(&x, 1, MPI_INT, i, 2, MPI_COMM_WORLD);
+			MPI_Send(&y, 1, MPI_INT, i, 2, MPI_COMM_WORLD);
+			MPI_Recv(&sum_extra, 1, MPI_INT, i, 2, MPI_COMM_WORLD, &status);
+		}
+		while (residual != 0)
+		{
+			x = a[a.size() - residual];
+			y = b[b.size() - residual];
+			MPI_Send(&x, 1, MPI_INT, residual, 2, MPI_COMM_WORLD);
+			MPI_Send(&y, 1, MPI_INT, residual, 2, MPI_COMM_WORLD);
+			MPI_Recv(&sum_extra, 1, MPI_INT, residual, 2, MPI_COMM_WORLD, &status);
+			sum += sum_extra;
+			residual = residual - 1;
+		}
+		cout << "The scalar product of vectors a and b: "<<sum;
+	}
+	else
+	{
+		a.resize(n);
+		b.resize(n);
+		MPI_Recv(&a[(rank - 1) * n / (size - 1)], n / (size - 1), MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(&b[(rank - 1) * n / (size - 1)], n / (size - 1), MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		sum_proc = 0;
+		for (int i = 0; i < n; i++)
+		{
+			sum_proc += a[i]*b[i];
+		}
+		MPI_Send(&sum_proc, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+		MPI_Recv(&x, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+		MPI_Recv(&y, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+		sum_extra = x * y;
+		MPI_Send(&sum_extra, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+	}
+	MPI_Finalize();
+}
---
+++ Assignment8.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <mpi.h>
+#include <vector>
+#include <math.h>
+using namespace std;
+int main(int argc, char** argv)
+{
+	int rank, size;
+	vector<int> message;
+	double time_start, time_finish, T;
+	long int n = 1000000;
+	int N = 10;
+	unsigned int L;
+	for (int i = 0; i < n; i++)
+	{
+		message.push_back(1);
+	}
+	MPI_Status status;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	for (int j = 0; j < n+1; j=j*10)
+	{
+		if (rank == 0)
+		{
+			time_start = MPI_Wtime();
+		}
+		for (int i = 0; i < N; i++)
+		{
+			if (rank == 0)
+			{
+				message.resize(j);
+				MPI_Send(&message[0], j, MPI_INT, 1, 0, MPI_COMM_WORLD);
+				MPI_Recv(&message[0], j, MPI_INT, 1, 1, MPI_COMM_WORLD, &status);
+			}
+			if (rank == 1)
+			{
+				MPI_Recv(&message[0], j, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+				MPI_Send(&message[0], j, MPI_INT, 0, 1, MPI_COMM_WORLD);
+			}
+			time_finish = MPI_Wtime();
+		}
+		if (rank == 0)
+		{
+			L = j * sizeof(int);
+			T = (time_finish - time_start) / N;
+			if (j == 0)
+			{
+				cout << "Latency= " << T / 2 << " s" << endl;
+			}
+			cout << "Length= " << L <<" Bytes ; Time " << T << " s ; Bandwidth= " << 2 * L / (1024*1024 * T) << " MB/s" << endl;
+		}
+		if (j==0) j = j + 1;
+	}
+	MPI_Finalize();
+}
